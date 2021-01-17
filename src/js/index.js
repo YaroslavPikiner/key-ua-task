f742aba03db77953b4a747243ccc9d23ae450d8e
Yaroslav <yaroslav6066@gmail.com>
2021-01-17 22:18:09 +0200

    add adaptive menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,44 @@
-import '../scss/main.scss'
+import "../scss/main.scss";
 
-const mySwiper = new Swiper('.swiper-container', {
+const mySwiper = new Swiper(".swiper-container", {
   // Optional parameters
 
-  direction: 'horizontal',
+  direction: "horizontal",
   loop: true,
   slidesPerView: 2,
   spaceBetween: 40,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
   breakpoints: {
     // when window width is >= 375px
     374: {
       slidesPerView: 1,
       spaceBetween: 20,
       pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
+        el: ".swiper-pagination",
+        type: "bullets",
       },
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 40,
     },
     1440: {
       slidesPerView: 2,
       spaceBetween: 40,
-    }
+    },
   },
-  // Navigation arrows
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
   navigation: {
-    nextEl: '.slider__btn--next',
-    prevEl: '.slider__btn--prev',
+    nextEl: ".slider__btn--next",
+    prevEl: ".slider__btn--prev",
   },
-})
+});
+
+document.querySelector(".header__burger--btn").addEventListener("click", () => {
+  document.querySelector(".header__burger--btn")
+    .classList.toggle("header__navigation");
+});
